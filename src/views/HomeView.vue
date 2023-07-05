fac91dabbb9f599400cbb1a55c493fe5b0d18b30
Erik Bjäreholt <erik@bjareho.lt>
2023-07-05 15:08:19 +0200

    fix: fixed test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
     <h1 class="my-3">Welcome to the ActivityWatch Leaderboard</h1>
