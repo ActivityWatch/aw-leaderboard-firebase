d970bd256f75c374c1a63e5b0bf23a3e5f3648b3
brayo <vukubrian@gmail.com>
2024-02-26 10:46:32 +0300

    fix the failing test

---
+++ src/components/__tests__/SignupForm.spec.ts
@@ -0,0 +1,18 @@
+import { describe, it, expect } from 'vitest'
+import { createPinia } from 'pinia' // Import the createPinia function from the 'pinia' package
+
+import { mount } from '@vue/test-utils'
+import SignupForm from '../SignupForm.vue'
+
+describe('SignupForm', () => {
+  it('renders properly', () => {
+    const pinia = createPinia() // Create a Pinia instance
+    const wrapper = mount(SignupForm, { 
+      props: { msg: 'Signup Vitest' },
+      global: {
+        plugins: [pinia] // Use the Pinia instance as a plugin
+      }
+    })
+    expect(wrapper.text()).toContain('Sign Up')
+  })
+})
