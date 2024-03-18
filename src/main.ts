97f43193d3efcc9198309df63c9c3ed63393f427
brayo <vukubrian@gmail.com>
2024-03-18 21:12:19 +0300

    chore:remove unused import

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,12 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-import { PiniaFirestoreSync } from 'pinia-plugin-firestore-sync'
 
 import App from './App.vue'
 import router from './router'
 
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
-pinia.use(PiniaFirestoreSync)
 
 const app = createApp(App)
 app.use(pinia)
