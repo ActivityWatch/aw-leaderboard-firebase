8a6e9b753f278e023da5abd7c5d6ac585deeff9c
brayo <vukubrian@gmail.com>
2024-03-22 16:45:43 +0300

    chore:prettier formatted

--- src/stores/leaderboard.ts
+++ src/stores/leaderboard.ts
@@ -1,26 +1,27 @@
+
 import { defineStore } from 'pinia'
 import type { ScreenTimeSummary } from '@/types'
 import { getLeaderboard } from '@/firebase/data'
 import { ref } from 'vue'
 
 export const useLeaderboardStore = defineStore('leaderboard', {
-  state: () => ({
-    leaderboardData: ref<ScreenTimeSummary[] | null>(null)
-  }),
-  getters: {
-    leaderboardExists: (state) => Boolean(state.leaderboardData),
-    getleaderboardData: (state) => state.leaderboardData
-  },
-  persist: true,
-  actions: {
-    async fetchLeaderboardData() {
-      getLeaderboard().then((data) => {
-        this.leaderboardData = data
-      })
+    state: () => ({
+        leaderboardData: ref<ScreenTimeSummary[] | null>(null),
+    }),
+    getters: {
+        leaderboardExists: (state) => Boolean(state.leaderboardData),
+        getleaderboardData: (state) => state.leaderboardData
     },
-    async resetStore() {
-      this.leaderboardData = null
-      this.$reset()
+    persist: true,
+    actions: {
+        async fetchLeaderboardData() {
+            getLeaderboard().then((data) => {
+                this.leaderboardData = data
+            })
+        },
+        async resetStore() {
+            this.leaderboardData = null
+            this.$reset()
+        }
     }
-  }
 })
