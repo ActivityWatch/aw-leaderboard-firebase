122f0883d6a88a3a87b6699491b57cf8782d911c
brayo <vukubrian@gmail.com>
2024-06-24 19:16:51 +0300

    feat: add an apikey component

--- src/stores/apikey.ts
+++ src/stores/apikey.ts
@@ -3,20 +3,14 @@ import { getApiKey } from '@/firebase/data'
 import { useAuthStore } from './auth'
 import { ref } from 'vue'
 
-export const useApiKeyStore = defineStore('apikey', {
-  state: () => ({
-    apikey : ref<string | null>(null)
-  }),
-  getters: {
-    apikeyExists: (state) => Boolean(state.apikey),
-    getApikey: (state) => state.apikey,
-  },
-  persist: false,
-  actions: {
-    async fetchKey() {
-      const userId = useAuthStore().user!.uid
-      const key = await getApiKey(userId);
-      this.apikey = key;
-        }
-      },
-  })
+export const useApiKeyStore = defineStore('apikey', () => {
+  const apikey = ref(null as string | null)
+  async function fetchKey() {
+    const userId = useAuthStore().user!.uid
+    getApiKey(userId).then((key) => {
+    apikey.value = key;
+    })
+  }
+
+  return { apikey, fetchKey }
+})
