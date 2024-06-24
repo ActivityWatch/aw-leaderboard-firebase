84d32d4955487a2004f9b6a3214535c28ff1eb9c
brayo <vukubrian@gmail.com>
2024-06-24 20:26:16 +0300

    feat: reactivity improvements

--- src/stores/screentime.ts
+++ src/stores/screentime.ts
@@ -5,42 +5,32 @@ import { useAuthStore } from './auth'
 import { getFirestore, collection, onSnapshot } from 'firebase/firestore'
 import { ref } from 'vue'
 
-export const useScreenTimeStore = defineStore('screentime', {
-  state: () => ({
-    screenTimeData: ref<ScreenTimeData[] | null>(null),
-    summary: ref<ScreenTimeSummary[] | null>(null)
-  }),
-  getters: {
-    summaryExists: (state) => Boolean(state.summary),
-    screenTimeExists: (state) => Boolean(state.screenTimeData),
-    getSummaries: (state) => state.summary,
-    getScreenTimeData: (state) => state.screenTimeData
-  },
-  persist: true,
-  actions: {
-    async fetchSummary() {
-      const userId = useAuthStore().user!.uid
-      const db = getFirestore()
-      const colRef = collection(db, `screentime/${userId}/${userId}`)
-      onSnapshot(colRef, (snapshot) => {
-        const changes = snapshot.docChanges()
-        if (changes.length === 0) return
-        console.log('updated screenTimeData')
-        getScreenTimeData(userId).then((data) => {
-          console.log('data', data)
-          this.screenTimeData = data
-          if (this.screenTimeData) {
-            this.summary = this.screenTimeData.map((data) => dataToSummary(data))
-          } else {
-            this.summary = null
-          }
-        })
+
+export const useScreenTimeStore = defineStore('screentime', () => {
+  const screenTimeData = ref<ScreenTimeData[] | []>([])
+  const summary = ref<ScreenTimeSummary[] | []>([])
+
+  async function fetchSummary() {
+    const userId = useAuthStore().user!.uid
+    const db = getFirestore()
+    const colRef = collection(db, `screentime/${userId}/${userId}`)
+    onSnapshot(colRef, (snapshot) => {
+      const changes = snapshot.docChanges()
+      if (changes.length === 0) return
+      console.log('updated screenTimeData')
+      getScreenTimeData(userId).then((data) => {
+        console.log('data', data)
+        screenTimeData.value = data
+        if (screenTimeData.value) {
+          summary.value = screenTimeData.value.map((data) => dataToSummary(data))
+        } else {
+          summary.value = []
+        }
       })
-    },
-    async resetStore() {
-      this.screenTimeData = null
-      this.summary = null
-      this.$reset()
-    }
+    })
   }
+
+  return { screenTimeData, summary, fetchSummary }
+}, {
+  persist: true
 })
