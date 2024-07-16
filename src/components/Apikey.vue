54c591358178eeb60a277e8015ff352490d23293
Brayo <vukubrian@gmail.com>
2024-07-16 11:57:45 +0300

    chore: remove unused import

--- src/components/Apikey.vue
+++ src/components/Apikey.vue
@@ -8,7 +8,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue'
+import { computed } from 'vue'
 import AWLHeader from '@/components/Header.vue'
 import { useApiKeyStore } from '@/stores/apikey'
 
