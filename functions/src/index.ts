9f38b92570af2761fc3de77c710131873ee5ff19
Brayo <vukubrian@gmail.com>
2024-07-18 10:50:02 +0300

    chore: eslint formatting

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,37 +1,37 @@
-import { onRequest } from 'firebase-functions/v2/https'
-import { onObjectFinalized } from 'firebase-functions/v2/storage'
-import { onSchedule } from 'firebase-functions/v2/scheduler'
-import * as admin from 'firebase-admin'
-import * as functions from 'firebase-functions'
-import * as genKey from 'generate-api-key'
-import { info, debug, error } from 'firebase-functions/logger'
-import { RawEvent, Event } from './types'
+import {onRequest} from "firebase-functions/v2/https";
+import {onObjectFinalized} from "firebase-functions/v2/storage";
+import {onSchedule} from "firebase-functions/v2/scheduler";
+import * as admin from "firebase-admin";
+import * as functions from "firebase-functions";
+import * as genKey from "generate-api-key";
+import {info, debug, error} from "firebase-functions/logger";
+import {RawEvent, Event} from "./types";
 
-admin.initializeApp()
+admin.initializeApp();
 
 exports.onUserCreated = functions.auth.user().onCreate((user) => {
-  info('User created: ', user.uid)
-  const db = admin.firestore()
-  const colpath = 'users'
-  const docpath = user.uid
-  const apiKey = genKey.generateApiKey()
-  const jsonObj = { apiKey: apiKey }
+  info("User created: ", user.uid);
+  const db = admin.firestore();
+  const colpath = "users";
+  const docpath = user.uid;
+  const apiKey = genKey.generateApiKey();
+  const jsonObj = {apiKey: apiKey};
   // TODO: handle possible collisions
-  return db.collection(colpath).doc(docpath).set(jsonObj)
-})
+  return db.collection(colpath).doc(docpath).set(jsonObj);
+});
 
 exports.onUserDeleted = functions.auth.user().onDelete((user) => {
   // TODO: consider what to do with the user's data
-  info('User deleted: ', user.uid)
-  const db = admin.firestore()
-  const colpath = 'users'
-  const docpath = user.uid
-  return db.collection(colpath).doc(docpath).delete()
-})
+  info("User deleted: ", user.uid);
+  const db = admin.firestore();
+  const colpath = "users";
+  const docpath = user.uid;
+  return db.collection(colpath).doc(docpath).delete();
+});
 
-export const UpdateLeaderboardData = onSchedule('every day 00:00', async (_) => {
+export const UpdateLeaderboardData = onSchedule("every day 00:00", async () => {
   // WIP
-  info('Updating leaderboard data')
+  info("Updating leaderboard data");
   const userScreenTimes = new Map<string, number>();
   const db = admin.firestore();
   const destinationColpath = "leaderboard";
@@ -40,7 +40,9 @@ export const UpdateLeaderboardData = onSchedule('every day 00:00', async (_) => 
   const promises = [];
   for (const doc of screenTimeDocs) {
     const userId: string = doc.id;
-    const userScreenTime = await db.collection(screentimeColpath + userId + "/" + userId).listDocuments();
+    const userScreenTime = await db
+      .collection(screentimeColpath + userId + "/" + userId)
+      .listDocuments();
     let total = 0;
     for (const day of userScreenTime) {
       const dayData = await day.get();
@@ -52,11 +54,13 @@ export const UpdateLeaderboardData = onSchedule('every day 00:00', async (_) => 
     }
     userScreenTimes.set(userId, total);
   }
-  const sortedUserScreenTimes = new Map([...userScreenTimes.entries()].sort((a, b) => b[1] - a[1]));
+  const sortedUserScreenTimes = new Map([...userScreenTimes.entries()]
+    .sort((a, b) => b[1] - a[1]));
   let rank = 1;
   for (const [userId, total] of sortedUserScreenTimes) {
     const jsonObj = JSON.parse(`{
     "userId": "${userId}",
+    "rank": ${rank},
     "total": ${total}
     }`);
     const promise = db.collection(destinationColpath).doc(userId).set(jsonObj);
@@ -64,142 +68,146 @@ export const UpdateLeaderboardData = onSchedule('every day 00:00', async (_) => 
     rank++;
   }
   await Promise.all(promises);
-})
+});
 export const getApiKey = functions.https.onCall(async (_, context) => {
   /** A callable function only executed when the user is logged in */
-  info('Getting ApiKey')
-  debug('Request data: ', context)
+  info("Getting ApiKey");
+  debug("Request data: ", context);
   if (!context.auth) {
-    error('Not authenticated')
-    return { error: 'Not authenticated' }
+    error("Not authenticated");
+    return {error: "Not authenticated"};
   }
-  const db = admin.firestore()
-  const colpath = 'users'
-  const docpath = context.auth?.uid
-  const docref = db.collection(colpath).doc(docpath)
-  const doc = await docref.get()
+  const db = admin.firestore();
+  const colpath = "users";
+  const docpath = context.auth?.uid;
+  const docref = db.collection(colpath).doc(docpath);
+  const doc = await docref.get();
   if (doc.exists && doc.data() && doc.data()?.apiKey) {
-    info('ApiKey found and sent to client')
-    return { apiKey: doc.data()?.apiKey }
+    info("ApiKey found and sent to client");
+    return {apiKey: doc.data()?.apiKey};
   } else {
-    const apiKey = genKey.generateApiKey()
-    await docref.set({ apiKey: apiKey })
-    info('ApiKey set and sent to client')
-    return { apiKey: apiKey }
+    const apiKey = genKey.generateApiKey();
+    await docref.set({apiKey: apiKey});
+    info("ApiKey set and sent to client");
+    return {apiKey: apiKey};
   }
-})
+});
 export const rotateApiKey = functions.https.onCall(async (_, context) => {
   /** A callable function only executed when the user is logged in */
-  info('Rotating ApiKey')
-  debug('Request data: ', context)
+  info("Rotating ApiKey");
+  debug("Request data: ", context);
   if (!context.auth) {
-    error('Not authenticated')
-    return { error: 'Not authenticated' }
+    error("Not authenticated");
+    return {error: "Not authenticated"};
   }
-  const db = admin.firestore()
-  const colpath = 'apiKeys'
-  const docpath = context.auth?.uid
-  const docref = db.collection(colpath).doc(docpath)
-  const doc = await docref.get()
+  const db = admin.firestore();
+  const colpath = "apiKeys";
+  const docpath = context.auth?.uid;
+  const docref = db.collection(colpath).doc(docpath);
+  const doc = await docref.get();
   if (doc.exists && doc.data() && doc.data()?.apiKey) {
-    const apiKey = genKey.generateApiKey()
-    await docref.update({ apiKey: apiKey })
-    info('ApiKey rotated and sent to client')
-    return { apiKey: apiKey }
+    const apiKey = genKey.generateApiKey();
+    await docref.update({apiKey: apiKey});
+    info("ApiKey rotated and sent to client");
+    return {apiKey: apiKey};
   } else {
-    await docref.set({ apiKey: genKey.generateApiKey() })
-    info('ApiKey set and sent to client')
-    return { apiKey: doc.data()?.apiKey }
+    await docref.set({apiKey: genKey.generateApiKey()});
+    info("ApiKey set and sent to client");
+    return {apiKey: doc.data()?.apiKey};
   }
-})
+});
 
 exports.uploadData = onRequest(async (request, response) => {
-  info('Storing data')
-  debug('Request data: ', request.body)
+  info("Storing data");
+  debug("Request data: ", request.body);
   if (!request.body.apiKey) {
-    error('No apiKey provided!')
-    response.status(400).send({ error: 'No apiKey provided!' })
-    return
+    error("No apiKey provided!");
+    response.status(400).send({error: "No apiKey provided!"});
+    return;
   }
   if (!request.body.data) {
-    error('No data provided to store!')
-    response.status(400).send({ error: 'No data provided!' })
-    return
+    error("No data provided to store!");
+    response.status(400).send({error: "No data provided!"});
+    return;
   }
 
-  const apiKey = request.body.apiKey
-  const db = admin.firestore()
-  const querySnapshot = await db.collection('users').where('apiKey', '==', apiKey).get()
-  debug('QuerySnapshot: ', querySnapshot)
+  const apiKey = request.body.apiKey;
+  const db = admin.firestore();
+  const querySnapshot = await db.collection("users")
+    .where("apiKey", "==", apiKey).get();
+  debug("QuerySnapshot: ", querySnapshot);
   if (querySnapshot.empty) {
-    error('Invalid apiKey provided!')
-    response.status(403).send({ error: 'Invalid apiKey provided!' })
-    return
+    error("Invalid apiKey provided!");
+    response.status(403).send({error: "Invalid apiKey provided!"});
+    return;
   }
-  const userId = querySnapshot.docs[0].id
-  const bucket = admin.storage().bucket()
-  const dataToStore = request.body.data
+  const userId = querySnapshot.docs[0].id;
+  const bucket = admin.storage().bucket();
+  const dataToStore = request.body.data;
 
-  const filename = `${userId}/${Date.now()}.json`
-  const file = bucket.file(filename)
-  await file.save(dataToStore)
+  const filename = `${userId}/${Date.now()}.json`;
+  const file = bucket.file(filename);
+  await file.save(dataToStore);
 
-  response.send({ message: 'Data stored successfully!' })
-})
+  response.send({message: "Data stored successfully!"});
+});
 
-exports.onUploadData = onObjectFinalized({ cpu: 4, memory: '2GiB' }, async (event) => {
-  info('Processing uploaded data')
-  const file = event.data
-  const bucket = admin.storage().bucket()
-  const data = await bucket.file(file.name).download()
-  const userId = file.name.split('/')[0]
-  const dataString = data.toString()
-  const jsonData: [RawEvent] = JSON.parse(dataString)
-  const db = admin.firestore()
-  const batch = db.batch()
-  const colpath = `screentime/${userId}/${userId}`
-  const promises = []
-  const dateMap = new Map<string, Event[]>()
-  for (const rawEvent of jsonData) {
+exports.onUploadData = onObjectFinalized(
+  {cpu: 4, memory: "2GiB"},
+  async (event) => {
+    info("Processing uploaded data");
+    const file = event.data;
+    const bucket = admin.storage().bucket();
+    const data = await bucket.file(file.name).download();
+    const userId = file.name.split("/")[0];
+    const dataString = data.toString();
+    const jsonData: [RawEvent] = JSON.parse(dataString);
+    const db = admin.firestore();
+    const batch = db.batch();
+    const colpath = `screentime/${userId}/${userId}`;
+    const promises = [];
+    const dateMap = new Map<string, Event[]>();
+    for (const rawEvent of jsonData) {
     // reduce from type RawEvent to Event
-    const event: Event = {
-      timestamp: rawEvent.timestamp,
-      duration: rawEvent.duration,
-      data: rawEvent.data,
-      category: rawEvent.data.$category ? rawEvent.data.$category : 'Uncategorized'
-    }
+      const event: Event = {
+        timestamp: rawEvent.timestamp,
+        duration: rawEvent.duration,
+        data: rawEvent.data,
+        category: rawEvent.data.$category ?
+          rawEvent.data.$category : "Uncategorized",
+      };
 
-    let date = new Date(event.timestamp).toISOString().split('T')[0]
-    date = date.replace(/\//g, '-')
-    if (dateMap.has(date)) {
-      dateMap.get(date)?.push(event)
-    } else {
-      dateMap.set(date, [event])
+      let date = new Date(event.timestamp).toISOString().split("T")[0];
+      date = date.replace(/\//g, "-");
+      if (dateMap.has(date)) {
+        dateMap.get(date)?.push(event);
+      } else {
+        dateMap.set(date, [event]);
+      }
     }
-  }
-  const dates = dateMap.entries()
-  for (const [date, events] of dates) {
-    const promise = db
-      .collection(colpath)
-      .doc(date)
-      .get()
-      .then((doc) => {
-        if (doc.exists) {
-          const events = doc.data()?.events as Event[]
-          events.push(...events)
-          batch.update(doc.ref, { events: events })
-        } else {
-          batch.set(db.collection(colpath).doc(date), {
-            events: [...events],
-            userId: userId,
-            date: date,
-            public: true
-          })
-        }
-      })
-    promises.push(promise)
-  }
-  await Promise.all(promises)
-  await batch.commit()
-  info('Data processed successfully')
-})
+    const dates = dateMap.entries();
+    for (const [date, events] of dates) {
+      const promise = db
+        .collection(colpath)
+        .doc(date)
+        .get()
+        .then((doc) => {
+          if (doc.exists) {
+            const events = doc.data()?.events as Event[];
+            events.push(...events);
+            batch.update(doc.ref, {events: events});
+          } else {
+            batch.set(db.collection(colpath).doc(date), {
+              events: [...events],
+              userId: userId,
+              date: date,
+              public: true,
+            });
+          }
+        });
+      promises.push(promise);
+    }
+    await Promise.all(promises);
+    await batch.commit();
+    info("Data processed successfully");
+  });
--- functions/src/types.ts
+++ functions/src/types.ts
@@ -39,7 +39,7 @@ export interface ChartData {
 }
 
 export interface Rule {
-  type: 'regex' | 'none'
+  type: "regex" | "none"
   regex?: RegExp
   ignore_case?: boolean
 }
@@ -58,65 +58,65 @@ export interface Category {
 
 export const defaultCategories: Category[] = [
   {
-    name: ['Work'],
-    rule: { type: 'regex', regex: /Google Docs|libreoffice|ReText/gi }
+    name: ["Work"],
+    rule: {type: "regex", regex: /Google Docs|libreoffice|ReText/gi},
   },
   {
-    name: ['Work', 'Programming'],
+    name: ["Work", "Programming"],
     rule: {
-      type: 'regex',
+      type: "regex",
       // eslint-disable-next-line max-len
-      regex: /GitHub|Stack Overflow|BitBucket|Gitlab|vim|Spyder|kate|Ghidra|Scite/gi
-    }
+      regex: /GitHub|Stack Overflow|BitBucket|Gitlab|vim|Spyder|kate|Ghidra|Scite/gi,
+    },
   },
   {
-    name: ['Work', 'Programming', 'ActivityWatch'],
-    rule: { type: 'regex', regex: /ActivityWatch|aw-/gi, ignore_case: true }
+    name: ["Work", "Programming", "ActivityWatch"],
+    rule: {type: "regex", regex: /ActivityWatch|aw-/gi, ignore_case: true},
   },
-  { name: ['Work', 'Image'], rule: { type: 'regex', regex: /GIMP|Inkscape/gi } },
-  { name: ['Work', 'Video'], rule: { type: 'regex', regex: /Kdenlive/gi } },
-  { name: ['Work', 'Audio'], rule: { type: 'regex', regex: /Audacity/gi } },
-  { name: ['Work', '3D'], rule: { type: 'regex', regex: /Blender/gi } },
+  {name: ["Work", "Image"], rule: {type: "regex", regex: /GIMP|Inkscape/gi}},
+  {name: ["Work", "Video"], rule: {type: "regex", regex: /Kdenlive/gi}},
+  {name: ["Work", "Audio"], rule: {type: "regex", regex: /Audacity/gi}},
+  {name: ["Work", "3D"], rule: {type: "regex", regex: /Blender/gi}},
   {
-    name: ['Media', 'Games'],
-    rule: { type: 'regex', regex: /Minecraft|RimWorld/gi }
-  },
-  {
-    name: ['Media', 'Video'],
-    rule: { type: 'regex', regex: /YouTube|Plex|VLC/gi }
+    name: ["Media", "Games"],
+    rule: {type: "regex", regex: /Minecraft|RimWorld/gi},
   },
   {
-    name: ['Media', 'Social Media'],
+    name: ["Media", "Video"],
+    rule: {type: "regex", regex: /YouTube|Plex|VLC/gi},
+  },
+  {
+    name: ["Media", "Social Media"],
     rule: {
-      type: 'regex',
+      type: "regex",
       regex: /reddit|Facebook|Twitter|Instagram|devRant/gi,
-      ignore_case: true
-    }
+      ignore_case: true,
+    },
   },
   {
-    name: ['Media', 'Music'],
+    name: ["Media", "Music"],
     rule: {
-      type: 'regex',
+      type: "regex",
       regex: /Spotify|Deezer/gi,
-      ignore_case: true
-    }
+      ignore_case: true,
+    },
   },
   {
-    name: ['Comms'],
+    name: ["Comms"],
     rule: {
-      type: 'regex',
-      regex: /Slack|Riot|Element|Discord|Nheko|NeoChat|Mattermost/gi
-    }
+      type: "regex",
+      regex: /Slack|Riot|Element|Discord|Nheko|NeoChat|Mattermost/gi,
+    },
   },
   {
-    name: ['Comms', 'IM'],
+    name: ["Comms", "IM"],
     rule: {
-      type: 'regex',
-      // eslint-disable-next-line max-len
-      regex:
-        /Messenger|Telegram|Signal|WhatsApp|Rambox|Slack|Riot|Element|Discord|Nheko|NeoChat|Mattermost/gi
-    }
+      type: "regex",
+
+      regex: // eslint-disable-next-line max-len
+        /Messenger|Telegram|Signal|WhatsApp|Rambox|Slack|Riot|Element|Discord|Nheko|NeoChat|Mattermost/gi,
+    },
   },
   // eslint-disable-next-line max-len
-  { name: ['Comms', 'Email'], rule: { type: 'regex', regex: /Gmail|Thunderbird|mutt|alpine/gi } }
-]
+  {name: ["Comms", "Email"], rule: {type: "regex", regex: /Gmail|Thunderbird|mutt|alpine/gi}},
+];
