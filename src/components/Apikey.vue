a4a2cd9f483add350671980b4ead60739376c2bf
Brayo <vukubrian@gmail.com>
2024-07-25 15:00:29 +0300

    feat: add copy to clipboard functionality and display a snackbar pop up

--- src/components/Apikey.vue
+++ src/components/Apikey.vue
@@ -2,19 +2,31 @@
   <AWLHeader />
   <div>
     <h1>API Key</h1>
-    <p>Your API key is: {{ apiKey }}</p>
+    <p>Your API key is: {{ apiKey }} <ContentCopyIcon @click ="copyApikey"/></p>
+    <SnackBarVue :message="snackbarMessage" ref="snackbar"/>
   </div>
-  <!--TODO: Add rotate key, copy to clipboard functionality -->
+  <!--TODO: Add rotate key -->
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import AWLHeader from '@/components/Header.vue'
 import { useApiKeyStore } from '@/stores/apikey'
+import ContentCopyIcon from 'vue-material-design-icons/ContentCopy.vue'
+import SnackBarVue from './SnackBar.vue'
+
+const snackbarMessage = ref('')
 
 const store = useApiKeyStore()
 store.fetchKey()
 const apiKey = computed(() => store.apikey)
+
+const copyApikey = () => {
+  if (!apiKey.value) return
+  navigator.clipboard.writeText(apiKey.value)
+  snackbarMessage.value = 'ApiKey copied to clipboard'
+}
+
 </script>
 
 <style scoped></style>
